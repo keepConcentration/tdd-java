14ebe721c87643b42b9a1fbd9cdb74448805f03e
keepConcentration <phm543@gmail.com>
2025-10-22 00:44:50 +0900

    test: 포인트 정상 조회(service) 테스트 코드 추가

---
+++ src/test/java/io/hhplus/tdd/point/domain/service/PointServiceTest.java
@@ -0,0 +1,40 @@
+package io.hhplus.tdd.point.domain.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+import io.hhplus.tdd.point.domain.model.UserPoint;
+import io.hhplus.tdd.point.domain.repository.UserPointRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class PointServiceTest {
+
+  @InjectMocks
+  private PointService pointService;
+
+  @Mock
+  private UserPointRepository userPointRepository;
+
+  @Test
+  @DisplayName("특정 회원의 포인트를 조회한다.")
+  void read() {
+    // given
+    long userId = 1L;
+    UserPoint userPoint = UserPoint.of(userId, 100L);
+    when(userPointRepository.read(anyLong()))
+        .thenReturn(userPoint);
+
+    // when
+    UserPoint readUserPoint = pointService.read(userId);
+
+    // then
+    assertEquals(userPoint, readUserPoint);
+  }
+}
