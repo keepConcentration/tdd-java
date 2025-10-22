3dac8d8e85b3f6783ca4eaf21ef7d28d5bf16825
keepConcentration <phm543@gmail.com>
2025-10-22 19:57:41 +0900

    test: 포인트 사용 이력 저장(application) 테스트코드 작성 (Red)

---
+++ src/test/java/io/hhplus/tdd/point/application/PointUsingServiceTest.java
@@ -0,0 +1,47 @@
+package io.hhplus.tdd.point.application;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import io.hhplus.tdd.point.domain.model.UserPoint;
+import io.hhplus.tdd.point.domain.service.PointHistoryService;
+import io.hhplus.tdd.point.domain.service.PointService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class PointUsingServiceTest {
+
+  @InjectMocks
+  private PointUsingService pointUsingService;
+
+  @Mock
+  private PointService pointService;
+
+  @Mock
+  private PointHistoryService pointHistoryService;
+
+  @Test
+  @DisplayName("포인트 사용 시 포인트 변경 이력 정보를 저장한다.")
+  void useAndSaveHistory() {
+    // given
+    long userId = 1L;
+    long amount = 100L;
+    UserPoint userPoint = UserPoint.of(userId, 1000L);
+    when(pointService.read(anyLong()))
+        .thenReturn(userPoint);
+
+    // when
+    pointUsingService.use(userId, amount);
+
+    // then
+    verify(pointHistoryService, times(1)).save(any());
+  }
+}
