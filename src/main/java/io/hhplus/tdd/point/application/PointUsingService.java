440bc0a5a717e621b6c11efbd739ce3e5f160dd7
keepConcentration <phm543@gmail.com>
2025-10-22 19:59:24 +0900

    test: 포인트 사용 이력 저장(application) 구현 (Green)

--- src/main/java/io/hhplus/tdd/point/application/PointUsingService.java
+++ src/main/java/io/hhplus/tdd/point/application/PointUsingService.java
@@ -1,6 +1,8 @@
 package io.hhplus.tdd.point.application;
 
+import io.hhplus.tdd.point.domain.model.PointHistory;
 import io.hhplus.tdd.point.domain.model.UserPoint;
+import io.hhplus.tdd.point.domain.service.PointHistoryService;
 import io.hhplus.tdd.point.domain.service.PointService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,8 +13,12 @@ public class PointUsingService {
 
   private final PointService pointService;
 
+  private final PointHistoryService pointHistoryService;
+
   public void use(long userId, long amount) {
     UserPoint userPoint = pointService.read(userId);
     pointService.update(UserPoint.of(userId, userPoint.point() - amount));
+
+    pointHistoryService.save(PointHistory.forUse(userId, amount));
   }
 }
