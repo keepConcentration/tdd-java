4289d9b901110b247a7efd3777b68f5505140b15
keepConcentration <phm543@gmail.com>
2025-10-23 19:49:28 +0900

    refactor: 포인트 충전 동시성 코드 리팩토링

--- src/main/java/io/hhplus/tdd/point/domain/service/PointService.java
+++ src/main/java/io/hhplus/tdd/point/domain/service/PointService.java
@@ -3,6 +3,7 @@ package io.hhplus.tdd.point.domain.service;
 import io.hhplus.tdd.point.domain.model.UserPoint;
 import io.hhplus.tdd.point.domain.repository.UserPointRepository;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,7 @@ import org.springframework.stereotype.Service;
 public class PointService {
 
   private final UserPointRepository userPointRepository;
-  private final ConcurrentHashMap<Long, ReentrantLock> userLocks = new ConcurrentHashMap<>();
+  private final ConcurrentHashMap<Long, Lock> userLocks = new ConcurrentHashMap<>();
 
   public UserPoint read(long id) {
     return userPointRepository.read(id);
@@ -23,14 +24,10 @@ public class PointService {
   }
 
   public void executeWithLock(long userId, Runnable runnable) {
-    ReentrantLock lock;
-      if (!userLocks.containsKey(userId)) {
-          userLocks.put(userId, new ReentrantLock());
-      }
-      lock = userLocks.get(userId);
-      lock.lock();
+    Lock lock = userLocks.computeIfAbsent(userId, id -> new ReentrantLock());
+    lock.lock();
     try {
-        runnable.run();
+      runnable.run();
     } finally {
       lock.unlock();
     }
